06dc899b4304d74f7ef3b5c0778f39f74585b0b9
KEI$HO <133760530+N-Keisho@users.noreply.github.com>
2025-04-10 23:19:37 +0900

    WebGLビルド

---
+++ Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayVideo : MonoBehaviour
+{
+    // VideoPlayerコンポーネント
+    [SerializeField] private VideoPlayer _videoPlayer;
+
+    // StreamingAssetsの動画ファイルへのパス
+    [SerializeField] private string _streamingAssetsMoviePath;
+
+    private void Start()
+    {
+        // URL指定
+        _videoPlayer.source = VideoSource.Url;
+
+        // StreamingAssetsフォルダ配下のパスの動画をURLとして指定する
+        _videoPlayer.url = Application.streamingAssetsPath + "/" + _streamingAssetsMoviePath;
+
+        // 再生
+        _videoPlayer.Play();
+    }
+}

52673ffb8d7bc7463e141b2fa714aa4b796ca68c
KEI$HO <133760530+N-Keisho@users.noreply.github.com>
2025-04-22 21:10:36 +0900

    通路ポスターの実装

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Path : MonoBehaviour
 {
+    [Header("Stopper")]
     [SerializeField] private GameObject _stopperA1;
     [SerializeField] private GameObject _stopperA2;
     [SerializeField] private GameObject _stopperB1;
     [SerializeField] private GameObject _stopperB2;
+
+    [Header("Poster")]
+    [SerializeField] private SpriteRenderer _posterA;
+    [SerializeField] private SpriteRenderer _posterB;
+
+    [Header("Poster Sprite")]
+    [SerializeField] private Sprite _watch;
+    [SerializeField] private Sprite _follow;
     void Start()
     {
         _stopperA1.SetActive(false);
@@ -22,6 +31,9 @@ public class Path : MonoBehaviour
         _stopperA2.SetActive(true);
         _stopperB1.SetActive(false);
         _stopperB2.SetActive(true);
+
+        _posterA.sprite = null;
+        _posterB.sprite = _follow;
     }
 
     public void InA2()
@@ -30,6 +42,9 @@ public class Path : MonoBehaviour
         _stopperA2.SetActive(false);
         _stopperB1.SetActive(false);
         _stopperB2.SetActive(true);
+
+        _posterA.sprite = null;
+        _posterB.sprite = _follow;
     }
 
     public void InB1()
@@ -38,6 +53,9 @@ public class Path : MonoBehaviour
         _stopperA2.SetActive(true);
         _stopperB1.SetActive(false);
         _stopperB2.SetActive(true);
+
+        _posterA.sprite = _follow;
+        _posterB.sprite = null;
     }
     public void InB2()
     {
@@ -45,6 +63,9 @@ public class Path : MonoBehaviour
         _stopperA2.SetActive(true);
         _stopperB1.SetActive(true);
         _stopperB2.SetActive(false);
+
+        _posterA.sprite = _follow;
+        _posterB.sprite = null;
     }
 
     public void CheckA()
@@ -53,6 +74,8 @@ public class Path : MonoBehaviour
         _stopperA2.SetActive(true);
         _stopperB1.SetActive(false);
         _stopperB2.SetActive(true);
+
+        _posterA.sprite = _watch;
     }
     public void CheckB()
     {
@@ -60,6 +83,8 @@ public class Path : MonoBehaviour
         _stopperA2.SetActive(true);
         _stopperB1.SetActive(true);
         _stopperB2.SetActive(true);
+
+        _posterB.sprite = _watch;
     }
 
     public void Out(){

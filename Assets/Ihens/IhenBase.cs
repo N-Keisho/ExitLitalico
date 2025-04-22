91c5a7a5ad2a5e35055b05b12cc5f0d3bd7e809e
KEI$HO <133760530+N-Keisho@users.noreply.github.com>
2025-04-22 13:47:51 +0900

    異変の説明文の追加

--- Assets/Ihens/IhenBase.cs
+++ Assets/Ihens/IhenBase.cs
@@ -5,4 +5,6 @@ using UnityEngine;
 abstract public class IhenBase : MonoBehaviour
 {
     protected bool _ihenDo = false;
+    [SerializeField] protected string _explanation = "初期値";
+    public string Explanation { get { return _explanation; } }
 }

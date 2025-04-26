29bdf4324f91c3c598083d0461a6bc0ce5062915
KEI$HO <133760530+N-Keisho@users.noreply.github.com>
2025-04-27 00:05:32 +0900

    コマンド時に音を再生

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class InputManager : MonoBehaviour
     [SerializeField] private Config _config;
     [SerializeField] private IhenList _ihenList;
     [SerializeField] private CameraZoom _cameraZoom;
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _sysSound;
 
     [Header("Parameters")]
     [SerializeField] private float _waitTime = 1f; // 待機時間
@@ -34,12 +37,14 @@ public class InputManager : MonoBehaviour
         _gameInputs.Player.Zoom.canceled += _cameraZoom.OnZoom;
 
         _gameInputs.System.Cheat.started += _gameManager.OnCheat;
+        _gameInputs.System.Cheat.started += PlaySysSound;
 
         _gameInputs.System.Config.started += _config.OnConfig;
 
         _gameInputs.System.GameQuit.started += _config.OnGameQuit;
 
         _gameInputs.System.ResetIhenDone.started += _ihenList.ResetIhenDone;
+        _gameInputs.System.ResetIhenDone.started += PlaySysSound;
 
         Invoke("Enabled", _waitTime);
     }
@@ -52,5 +57,13 @@ public class InputManager : MonoBehaviour
     public void Dispose() 
     {
         _gameInputs?.Dispose();
+    }
+
+    private void PlaySysSound(InputAction.CallbackContext context)
+    {
+        if (_audioSource != null && _sysSound != null)
+        {
+            _audioSource.PlayOneShot(_sysSound);
+        }
     }
 }
